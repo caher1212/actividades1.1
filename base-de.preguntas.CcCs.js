89ddc76c68b74844ab2fd48526463dd97c3de123
caher1 <ch1212ceti@gmail.com>
2023-10-15 13:55:58 -0600

    Slide-Incomplete-style1.3

---
+++ base-de.preguntas.CcCs.js
@@ -0,0 +1,20 @@
+let basePreguntas = [
+    //Pregunta 1
+    {
+        pregunta: "En esto circuito las salidas dependen puramente de las entradas que este recibe...",
+        respuesta: "Combinacional",
+        distractores:["Secuencial", "", ""],
+    
+        },
+    //Pregunta 2
+     //Pregunta 2
+     {
+        pregunta: "La siguiente imagen es un ejemplo de un circuito Secuencial",
+        imagen: "./img/Secuencial.png",
+        respuesta: "Verdadero",
+        distractores:["Falso", "", ""],
+    
+        },
+        
+    
+];
